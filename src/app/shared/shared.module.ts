0c3b1ba13eb52128b82bb1a00e741175f5656817
bimbaquingoch <bryan_3871@hotmail.com>
2021-11-11 13:14:07 -0500

    router sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ], exports: [ SidebarComponent ],
   imports: [
-    CommonModule
+    CommonModule, RouterModule
   ]
 } )
 export class SharedModule { }
